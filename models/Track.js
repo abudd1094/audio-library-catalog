e596ac1292d72bf8948613826165000f9c9f3e7d
Alec Budd <abudd1094@gmail.com>
2019-05-13 17:19:50 +0200

    track model & track routes: create track & get user library

---
+++ models/Track.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Schema
+const TrackSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users', 
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  artist: {
+    type: String,
+    required: true
+  },
+  album: {
+    type: String
+  },
+  year: {
+    type: String
+  },
+  label: {
+    type: String
+  },
+  genre: {
+    type: String
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Track = mongoose.model('tracks', TrackSchema); // takes route name & schema
---
+++ routes/api/tracks.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../../middleware/auth');
+
+const { check, validationResult } = require('express-validator/check');
+
+// Load Models
+const Track = require('../../models/Track');
+const User = require('../../models/User');
+
+// @route   GET api/tracks/mylibrary
+// @desc    Get all tracks in a user's library
+// @access  Private
+router.get('/:user_id', auth, async (req, res) => {
+  try {
+    const tracks = await Track.find({ user: req.user.id })
+
+    res.json(tracks);
+
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send('Server error');
+  }
+})
+
+// @route   POST api/tracks
+// @desc    Add a track
+// @access  Private
+router.post(
+  '/',
+  auth, 
+  [
+    check('title', 'Please include a title').exists(),
+    check('artist', 'Please include an artist').exists()
+  ], 
+  async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) { // if there are errors, do this:
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const user = await User.findById(req.user.id).select('-password') 
+    const { title, artist, album, year, label, genre } = req.body; 
+
+    try {
+      // See if track already exists
+      let track = await Track.findOne({ title, artist });
+      if(track) {
+        return res.status(400).json({ errors: [{ msg: 'Track already exists' }] });
+      }
+
+      track = new Track({
+        user: req.user.id,
+        title,
+        artist,
+        album,
+        year,
+        label,
+        genre
+      })
+
+      await track.save()
+
+      res.json(track)
+
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send('Server error');
+    };
+  }
+)
+
+module.exports = router;
